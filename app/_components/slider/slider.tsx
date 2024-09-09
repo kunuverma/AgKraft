5ae4fbd82beb7cf24ab3ef0aebdf5f34c60cdde0
Kunal Verma <kunalverma@xcrino.com>
2024-09-09 23:41:21 +0530

    create file structure

--- app/_components/slider/slider.tsx
+++ app/_components/slider/slider.tsx
@@ -19,39 +19,39 @@ function Slider() {
 
     return (
         <div className=" bg-red-200 mt-28">
-        <div className="">
-            {/* Left Box */}
-            <div>
-                <h1>Slider</h1>
-            </div>
+            <div className="">
+                {/* Left Box */}
+                <div>
+                    <h1>Slider</h1>
+                </div>
 
-            {/* Right Box */}
-            <div>
-                <Carousel
-                    plugins={[plugin.current]}
-                    className="w-full max-w-xs"
-                    onMouseEnter={plugin.current.stop}
-                    onMouseLeave={plugin.current.reset}
-                >
-                    <CarouselContent>
-                        {Array.from({ length: 5 }).map((_, index) => (
-                            <CarouselItem key={index}>
-                                <div className="p-1">
-                                    <Card>
-                                        <CardContent className="flex aspect-square items-center justify-center p-6">
-                                            <span className="text-4xl font-semibold">{index + 1}</span>
-                                        </CardContent>
-                                    </Card>
-                                </div>
-                            </CarouselItem>
-                        ))}
-                    </CarouselContent>
-                    <CarouselPrevious />
-                    <CarouselNext />
-                </Carousel>
-            </div>
+                {/* Right Box */}
+                <div>
+                    <Carousel
+                        plugins={[plugin.current]}
+                        className="w-full max-w-xs"
+                        onMouseEnter={plugin.current.stop}
+                        onMouseLeave={plugin.current.reset}
+                    >
+                        <CarouselContent>
+                            {Array.from({ length: 5 }).map((_, index) => (
+                                <CarouselItem key={index}>
+                                    <div className="p-1">
+                                        <Card>
+                                            <CardContent className="flex aspect-square items-center justify-center p-6">
+                                                <span className="text-4xl font-semibold">{index + 1}</span>
+                                            </CardContent>
+                                        </Card>
+                                    </div>
+                                </CarouselItem>
+                            ))}
+                        </CarouselContent>
+                        <CarouselPrevious />
+                        <CarouselNext />
+                    </Carousel>
+                </div>
 
-        </div>
+            </div>
         </div>
 
     )
