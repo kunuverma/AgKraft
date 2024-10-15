6b2223f48d56a8fbe210ec3b04addf150b1e3948
kunal verma <coderk.verma10@gmail.com>
2024-10-15 22:30:26 +0530

    updated services page

--- app/_components/home/enquiry-from.tsx
+++ app/_components/home/enquiry-from.tsx
@@ -1,142 +1,195 @@
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
-import Image from 'next/image';
-import contact from "@/public/assest/CONTACT US.gif";  // Ensure this path is correct
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import Image from "next/image";
+import contact from "@/public/assest/CONTACT US.gif"; // Ensure this path is correct
+import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue,} from "@/components/ui/select";
 
 interface FormData {
-    firstName: string;
-    lastName: string;
-    email: string;
-    phone: string;
-    services: string;
-    message: string;
-    country?: string;
-    city?: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  message: string;
+  country?: string;
+  city?: string;
 }
 
 const schema = yup.object().shape({
-    firstName: yup.string().required('First Name is required'),
-    lastName: yup.string().required('Last Name is required'),
-    email: yup.string().email('Invalid email').required('Email is required'),
-    phone: yup
-        .string()
-        .matches(/^[0-9]{10}$/, 'Phone number must be exactly 10 digits')
-        .required('Phone number is required'),
-    services: yup.string().required('Please select a service'),
-    message: yup.string().required('Message is required'),
-    // country: yup.string().required('Country is required'),
-    // city: yup.string().required('City is required'),
+  firstName: yup.string().required("First Name is required"),
+  lastName: yup.string().required("Last Name is required"),
+  email: yup.string().email("Invalid email").required("Email is required"),
+  phone: yup
+    .string()
+    .matches(/^[0-9]{10}$/, "Phone number must be exactly 10 digits")
+    .required("Phone number is required"),
+  message: yup.string().required("Message is required"),
+  // country: yup.string().required('Country is required'),
+  // city: yup.string().required('City is required'),
 });
 
 const EnquiryForm = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm<FormData>({
-        resolver: yupResolver(schema),
-    });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: yupResolver(schema),
+  });
 
-    const onSubmit = (data: FormData) => {
-        console.log('Form Submitted:', data);
-    };
+  const onSubmit = (data: FormData) => {
+    console.log("Form Submitted:", data);
+  };
 
-    return (
-        <div className="flex flex-col justify-center gap-12 my-5 items-center mx-5 sm:mx-10 bg-[#191A1E] h-full">
-            <div className='flex items-center gap-2 sm:gap-3 mt-5'>
-                <span className="text-white whitespace-pre-wrap text-left relative z-20 mt-2 bg-clip-text text-3xl sm:text-4xl font-bold leading-none tracking-tighter text-transparent">
-                    Contact Us
-                </span>
-                <span className='whitespace-pre-wrap text-left relative z-20 mt-2 bg-clip-text text-3xl sm:text-4xl font-bold leading-none tracking-tighter text-transparent bg-gradient-to-b from-[#ffd319] via-[#ff2975] to-[#8c1eff]'>Today</span>
-            </div>
+  return (
+    <div className="flex flex-col justify-center gap-12 my-5 items-center mx-5 sm:mx-10 bg-[#191A1E] h-full">
+      <div className="flex items-center gap-2 sm:gap-3 mt-5">
+        <span className="text-white whitespace-pre-wrap text-left relative z-20 mt-2 bg-clip-text text-3xl sm:text-4xl font-bold leading-none tracking-tighter text-transparent">
+          Contact Us
+        </span>
+        <span className="whitespace-pre-wrap text-left relative z-20 mt-2 bg-clip-text text-3xl sm:text-4xl font-bold leading-none tracking-tighter text-transparent bg-gradient-to-b from-[#ffd319] via-[#ff2975] to-[#8c1eff]">
+          Today
+        </span>
+      </div>
 
-            <div className='flex flex-col md:flex-row gap-10 md:gap-36 rounded-xl px-4 md:px-0 mb-5'>
-                {/* Image */}
-                <div className='hidden md:block rounded-xl'>
-                    <Image
-                        className="w-[450px] h-[450px] rounded-xl"
-                        src={contact} // Ensure the image path is correct
-                        alt="Contact Us Graphic"
-                    />
-                </div>
-
-                {/* Form */}
-                <form onSubmit={handleSubmit(onSubmit)} className="p-8 w-full max-w-lg bg-[#1B1C20] text-white rounded-lg shadow-md">
-                    <div className="grid grid-cols-2 gap-10 mb-6">
-                        <div>
-                            <input
-                                {...register('firstName')}
-                                className={`block w-full px-4 py-3 bg-transparent border-b ${errors.firstName ? 'border-red-500' : 'border-gray-600'} focus:outline-none`}
-                                placeholder="First Name"
-                            />
-                            {errors.firstName && <span className="text-red-500 text-sm">{errors.firstName.message}</span>}
-                        </div>
-                        <div>
-                            <input
-                                {...register('lastName')}
-                                className={`block w-full px-4 py-3 bg-transparent border-b ${errors.lastName ? 'border-red-500' : 'border-gray-600'} focus:outline-none`}
-                                placeholder="Last Name"
-                            />
-                            {errors.lastName && <span className="text-red-500 text-sm">{errors.lastName.message}</span>}
-                        </div>
-                    </div>
-
-                    {/* Email */}
-                    <div className="mb-6">
-                        <input
-                            {...register('email')}
-                            className={`block w-full px-4 py-3 bg-transparent border-b ${errors.email ? 'border-red-500' : 'border-gray-600'} focus:outline-none`}
-                            placeholder="Email"
-                        />
-                        {errors.email && <span className="text-red-500 text-sm">{errors.email.message}</span>}
-                    </div>
-
-                    {/* Phone */}
-                    <div className="mb-6">
-                        <input
-                            {...register('phone')}
-                            className={`block w-full px-4 py-3 bg-transparent border-b ${errors.phone ? 'border-red-500' : 'border-gray-600'} focus:outline-none`}
-                            placeholder="Phone Number"
-                        />
-                        {errors.phone && <span className="text-red-500 text-sm">{errors.phone.message}</span>}
-                    </div>
-
-                    {/* Country and City */}
-                    <div className="grid grid-cols-2 gap-8 mb-6">
-                        <div>
-                            <input
-                                {...register('country')}
-                                className={`block w-full px-4 py-3 bg-transparent border-b ${errors.country ? 'border-red-500' : ' border-gray-600'} focus:outline-none`}
-                                placeholder="Country"
-                            />
-                            {/* {errors.country && <span className="text-red-500 text-sm">{errors.country.message}</span>} */}
-                        </div>
-                        <div>
-                            <input
-                                {...register('city')}
-                                className={`block w-full px-4 py-3 bg-transparent border-b ${errors.city ? 'border-red-500' : ' border-gray-600'} focus:outline-none`}
-                                placeholder="City"
-                            />
-                            {/* {errors.city && <span className="text-red-500 text-sm">{errors.city.message}</span>} */}
-                        </div>
-                    </div>
-
-                    {/* Comments */}
-                    <div className="mb-6">
-                        <textarea
-                            {...register('message')}
-                            className="block w-full px-4 py-3 bg-transparent border-b border-gray-600 focus:outline-none"
-                            placeholder="Write your comments"
-                        />
-                    </div>
-
-                    {/* Submit Button */}
-                    <button
-                        type="submit"
-                        className="w-full py-3  mt-6 rounded-md bg-gradient-to-r from-[#FD503A] to-[#FE8449] hover:bg-gradient-to-r hover:from-[#bd4636] hover:to-[#ff723a] text-white font-bold transition duration-200 hover:bg-white hover:text-[#ffffff] border-2 border-transparent hover:border-[#FD3930]">
-                        ENQUIRY NOW
-                    </button>
-                </form>
-            </div>
+      <div className="flex flex-col md:flex-row gap-10 md:gap-36 rounded-xl px-4 md:px-0 mb-5">
+        {/* Image */}
+        <div className="hidden md:block rounded-xl">
+          <Image
+            className="w-[450px] h-[450px] rounded-xl"
+            src={contact} // Ensure the image path is correct
+            alt="Contact Us Graphic"
+          />
         </div>
-    );
+
+        {/* Form */}
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="p-0 md:p-8 w-full max-w-lg bg-[#1B1C20] text-white rounded-lg shadow-md"
+        >
+          <div className="grid grid-cols-2 gap-10 mb-6">
+            <div>
+              <input
+                {...register("firstName")}
+                className={`block w-full px-4 py-3 bg-transparent border-b ${
+                  errors.firstName ? "border-red-500" : "border-gray-600"
+                } focus:outline-none`}
+                placeholder="First Name"
+              />
+              {errors.firstName && (
+                <span className="text-red-500 text-sm">
+                  {errors.firstName.message}
+                </span>
+              )}
+            </div>
+            <div>
+              <input
+                {...register("lastName")}
+                className={`block w-full px-4 py-3 bg-transparent border-b ${
+                  errors.lastName ? "border-red-500" : "border-gray-600"
+                } focus:outline-none`}
+                placeholder="Last Name"
+              />
+              {errors.lastName && (
+                <span className="text-red-500 text-sm">
+                  {errors.lastName.message}
+                </span>
+              )}
+            </div>
+          </div>
+
+          {/* Email */}
+          <div className="mb-6">
+            <input
+              {...register("email")}
+              className={`block w-full px-4 py-3 bg-transparent border-b ${
+                errors.email ? "border-red-500" : "border-gray-600"
+              } focus:outline-none`}
+              placeholder="Email"
+            />
+            {errors.email && (
+              <span className="text-red-500 text-sm">
+                {errors.email.message}
+              </span>
+            )}
+          </div>
+
+          {/* Phone */}
+          <div className="mb-6">
+            <input
+              {...register("phone")}
+              className={`block w-full px-4 py-3 bg-transparent border-b ${
+                errors.phone ? "border-red-500" : "border-gray-600"
+              } focus:outline-none`}
+              placeholder="Phone Number"
+            />
+            {errors.phone && (
+              <span className="text-red-500 text-sm">
+                {errors.phone.message}
+              </span>
+            )}
+          </div>
+
+          {/* Services */}
+          <div className="mb-6">
+            <Select>
+              <SelectTrigger className={` w-full px-4 py-3 bg-transparent`}>
+                <SelectValue placeholder="Select Services" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="web">Web Development</SelectItem>
+                <SelectItem value="app">App Development</SelectItem>
+                <SelectItem value="soft">Software Development</SelectItem>
+                <SelectItem value="digital">Digital Marketing</SelectItem>
+                <SelectItem value="seo">SEO</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+
+          {/* Country and City */}
+          <div className="grid grid-cols-2 gap-8 mb-6">
+            <div>
+              <input
+                {...register("country")}
+                className={`block w-full px-4 py-3 bg-transparent border-b ${
+                  errors.country ? "border-red-500" : " border-gray-600"
+                } focus:outline-none`}
+                placeholder="Country"
+              />
+              {/* {errors.country && <span className="text-red-500 text-sm">{errors.country.message}</span>} */}
+            </div>
+            <div>
+              <input
+                {...register("city")}
+                className={`block w-full px-4 py-3 bg-transparent border-b ${
+                  errors.city ? "border-red-500" : " border-gray-600"
+                } focus:outline-none`}
+                placeholder="City"
+              />
+              {/* {errors.city && <span className="text-red-500 text-sm">{errors.city.message}</span>} */}
+            </div>
+          </div>
+
+          {/* Comments */}
+          <div className="mb-6">
+            <textarea
+              {...register("message")}
+              className="block w-full px-4 py-3 bg-transparent border-b border-gray-600 focus:outline-none"
+              placeholder="Write your comments"
+            />
+          </div>
+
+          {/* Submit Button */}
+          <button
+            type="submit"
+            className="w-full py-3  mt-6 rounded-md bg-gradient-to-r from-[#FD503A] to-[#FE8449] hover:bg-gradient-to-r hover:from-[#bd4636] hover:to-[#ff723a] text-white font-bold transition duration-200 hover:bg-white hover:text-[#ffffff] border-2 border-transparent hover:border-[#FD3930]"
+          >
+            ENQUIRY NOW
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default EnquiryForm;
